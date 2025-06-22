d5764c3f3388b4f5b1711e5474251c40f0a5084e
Aman Kumar Sharma <amankumarsharma01234@gmail.com>
2025-06-23 00:09:02 +0530

    Initial commit for main-app

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    port: 3000,
+  },
+  output: {
+    publicPath: "auto",
+    uniqueName: "main_app" 
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "main_app",
+      remotes: {
+        music_library: "music_library@http://localhost:3001/remoteEntry.js",
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
